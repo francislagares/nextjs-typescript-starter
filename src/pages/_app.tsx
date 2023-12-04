f3e4f0f97e87b7f04c006265498c435bacff2d1d
Francis Lagares <francis.lagares@gmail.com>
2023-12-04 19:02:39 +0100

    feat: upgrade yarn version

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-
-import '@/app/styles/globals.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- src/pages/_index.tsx
+++
@@ -1,5 +0,0 @@
-import type { NextPage } from 'next';
-
-const Home: NextPage = () => <main>Next.js 13 Typescript Starter</main>;
-
-export default Home;
